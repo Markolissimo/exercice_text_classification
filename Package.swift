9a7056325ef457e0f95c4ed50f15f03378fb0000
Markolissimo <mark.berezovskyi@gmail.com>
2026-04-24 16:24:36 +0300

    [fix]: Exclude App entrypoint from library target to fix duplicate main

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
     targets: [
         .target(
             name: "ExerciseClassifier",
-            dependencies: []
+            dependencies: [],
+            exclude: ["App"]
         )
     ]
 )
